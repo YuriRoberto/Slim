a63a1154878f0a4ce8e0fac22a391152cb7aaed0
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-10 14:07:53 -0300

    request e response

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,9 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 $app = new \Slim\App;
 
 $app->get('/hello', function(){
